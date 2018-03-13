2db045b6e0d2b604c080f13910042bf184ec132f
Jennifer Tristan <jt6161@cox.net>
2018-03-12 21:34:17 -0700

    Updated readme file.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-undefined
+Project Title: Churro Gonutz Order App
+
+Project Description: A fullstack app to order products from Churro Gonutz, a small business with custom made churro donuts. The app will feature photos and descriptions of the products as well as prices.
+
+This app will be used by customers to order products and employees of Churro Gonutz to prepare the order. This will make it easier for the customer and the food prep.
+
+Outputs will be the customer orders that are generated. These generated orders will be used to create the custom donut order to be paid for when it is picked up.
+
+Inputs will a form featuring products with customizable items that can be added. The customer can choose the size and toppings of the donuts, this will generate the order to be made by the staff.
+
+Technologies to be utilized: Front end framework-React, Client side routing-react-router, State management library-Redux, Stying-React-strap, Bootswatch
+
+Feature list / Stories:
+
+Nav bar - Home/Contact, Products, Cart
+
+As a user I go to the Home page and see a splash page with contact information on Churro GoNutz and logo. It will feature a photo of one of the products.
+
+As a user I go to the products page to view products in a list. Here I can click on a product to add it to my order(cart).
+
+As a user I go to my order(cart) page to see my order. As a user I can remove, add or delete items from my order until it is correct. I can send a message to the employees of Churro Gonutz to start to prepare my order.   
+
+As a user/employee of Churro Gonutz I can view my messages that contain customers orders. As a user/employee I can prep the orders for the customers to be picked up at the current location of Churro Gonutz.
--- db/seeds/01_products.js
+++ db/seeds/01_products.js
@@ -5,16 +5,16 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('products').insert([
-        { name: 'Churro Single Stack', description: 'Single Stack of Churro Gonutz', price: '3', img_url: 'img_url' },
-        { name: 'Churro Double Stack', description: 'Double Stack of Churro Gonutz', price: '5', img_url: 'img_url' },
-        { name: 'Churro Triple Stack', description: 'Triple Stack of Churro Gonutz', price: '7', img_url: 'img_url' },
-        { name: 'Churro Tower', description: 'Triple Stack of Churro Gonutz, two scoops of Vanilla Ice Cream, Chocolate drizzle with Whip Cream, Sprinkles and Maraschino Cherry.', price: '8', img_url: 'img_url' },
-        { name: 'Chu-Reo', description: 'Churro Gonut topped with Vanilla Ice Cream, Oreo Crumble and Chocolate drizzle.', price: '5', img_url: 'img_url' },
+        {name: 'Churro Single Stack', description: 'Single Stack of Churro Gonutz', price: '3', img_url: 'img_url' },
+        {name: 'Churro Double Stack', description: 'Double Stack of Churro Gonutz', price: '5', img_url: 'img_url' },
+        {name: 'Churro Triple Stack', description: 'Triple Stack of Churro Gonutz', price: '7', img_url: 'img_url' },
+        {name: 'Churro Tower', description: 'Triple Stack of Churro Gonutz, two scoops of Vanilla Ice Cream, Chocolate drizzle with Whip Cream, Sprinkles and Maraschino Cherry.', price: '8', img_url: 'img_url' },
+        {name: 'Chu-Reo', description: 'Churro Gonut topped with Vanilla Ice Cream, Oreo Crumble and Chocolate drizzle.', price: '5', img_url: 'img_url' },
 
-        { name: 'Spicy Churro', description: 'Churro', price: '7', img_url: 'img_url' },
-        { name: 'Churro Triple Stack', description: 'Triple Stack of Churro Gonutz', price: '7', img_url: 'img_url' },
-        { name: 'Churro Triple Stack', description: 'Triple Stack of Churro Gonutz', price: '7', img_url: 'img_url' },
-        { name: 'Churro Triple Stack', description: 'Triple Stack of Churro Gonutz', price: '7', img_url: 'img_url' }
+        {name: 'Spicy Churro', description: 'Churro', price: '7', img_url: 'img_url' },
+        {name: 'Churro Triple Stack', description: 'Triple Stack of Churro Gonutz', price: '7', img_url: 'img_url' },
+        {name: 'Churro Triple Stack', description: 'Triple Stack of Churro Gonutz', price: '7', img_url: 'img_url' },
+        {name: 'Churro Triple Stack', description: 'Triple Stack of Churro Gonutz', price: '7', img_url: 'img_url' }
       ]);
     });
 };
